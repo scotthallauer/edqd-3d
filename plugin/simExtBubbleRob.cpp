a36f7f9b6b7784ddcabb5421bf2acff41518174a
Scott Hallauer <scott.hallauer@gmail.com>
2021-01-11 18:00:40 +0200

    Add example plugin files for controller template

---
+++ plugin/simExtBubbleRob.cpp
@@ -0,0 +1,291 @@
+#include "simExtBubbleRob.h"
+#include "scriptFunctionData.h"
+#include <iostream>
+#include "simLib.h"
+
+#ifdef _WIN32
+    #ifdef QT_COMPIL
+        #include <direct.h>
+    #else
+        #include <shlwapi.h>
+        #pragma comment(lib, "Shlwapi.lib")
+    #endif
+#endif /* _WIN32 */
+#if defined (__linux) || defined (__APPLE__)
+    #include <unistd.h>
+    #define WIN_AFX_MANAGE_STATE
+#endif /* __linux || __APPLE__ */
+
+#define CONCAT(x,y,z) x y z
+#define strConCat(x,y,z)    CONCAT(x,y,z)
+
+#define PLUGIN_NAME "BubbleRob"
+
+static LIBRARY simLib;
+
+struct sBubbleRob
+{
+    int handle;
+    int motorHandles[2];
+    int sensorHandle;
+    float backRelativeVelocities[2];
+    bool run;
+    float backMovementDuration;
+};
+
+static std::vector<sBubbleRob> allBubbleRobs;
+static int nextBubbleRobHandle=0;
+
+int getBubbleRobIndexFromHandle(int bubbleRobHandle)
+{
+    for (unsigned int i=0;i<allBubbleRobs.size();i++)
+    {
+        if (allBubbleRobs[i].handle==bubbleRobHandle)
+            return(i);
+    }
+    return(-1);
+}
+
+// --------------------------------------------------------------------------------------
+// simExtBubble_create
+// --------------------------------------------------------------------------------------
+#define LUA_CREATE_COMMAND "simBubble.create"
+
+const int inArgs_CREATE[]={
+    3,
+    sim_script_arg_int32|sim_script_arg_table,2,
+    sim_script_arg_int32,0,
+    sim_script_arg_float|sim_script_arg_table,2,
+};
+
+void LUA_CREATE_CALLBACK(SScriptCallBack* cb)
+{
+    CScriptFunctionData D;
+    int handle=-1;
+    if (D.readDataFromStack(cb->stackID,inArgs_CREATE,inArgs_CREATE[0],LUA_CREATE_COMMAND))
+    {
+        std::vector<CScriptFunctionDataItem>* inData=D.getInDataPtr();
+        sBubbleRob bubbleRob;
+        handle=nextBubbleRobHandle++;
+        bubbleRob.handle=handle;
+        bubbleRob.motorHandles[0]=inData->at(0).int32Data[0];
+        bubbleRob.motorHandles[1]=inData->at(0).int32Data[1];
+        bubbleRob.sensorHandle=inData->at(1).int32Data[0];
+        bubbleRob.backRelativeVelocities[0]=inData->at(2).floatData[0];
+        bubbleRob.backRelativeVelocities[1]=inData->at(2).floatData[1];
+        bubbleRob.run=false;
+        allBubbleRobs.push_back(bubbleRob);
+    }
+    D.pushOutData(CScriptFunctionDataItem(handle));
+    D.writeDataToStack(cb->stackID);
+}
+// --------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------
+// simExtBubble_destroy
+// --------------------------------------------------------------------------------------
+#define LUA_DESTROY_COMMAND "simBubble.destroy"
+
+const int inArgs_DESTROY[]={
+    1,
+    sim_script_arg_int32,0,
+};
+
+void LUA_DESTROY_CALLBACK(SScriptCallBack* cb)
+{
+    CScriptFunctionData D;
+    bool success=false;
+    if (D.readDataFromStack(cb->stackID,inArgs_DESTROY,inArgs_DESTROY[0],LUA_DESTROY_COMMAND))
+    {
+        std::vector<CScriptFunctionDataItem>* inData=D.getInDataPtr();
+        int handle=inData->at(0).int32Data[0];
+        int index=getBubbleRobIndexFromHandle(handle);
+        if (index>=0)
+        {
+            allBubbleRobs.erase(allBubbleRobs.begin()+index);
+            success=true;
+        }
+        else
+            simSetLastError(LUA_DESTROY_COMMAND,"Invalid BubbleRob handle.");
+    }
+    D.pushOutData(CScriptFunctionDataItem(success));
+    D.writeDataToStack(cb->stackID);
+}
+// --------------------------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------------------------
+// simExtBubble_start
+// --------------------------------------------------------------------------------------
+#define LUA_START_COMMAND "simBubble.start"
+
+const int inArgs_START[]={
+    1,
+    sim_script_arg_int32,0,
+};
+
+void LUA_START_CALLBACK(SScriptCallBack* cb)
+{
+    CScriptFunctionData D;
+    bool success=false;
+    if (D.readDataFromStack(cb->stackID,inArgs_START,inArgs_START[0],LUA_START_COMMAND))
+    {
+        std::vector<CScriptFunctionDataItem>* inData=D.getInDataPtr();
+        int handle=inData->at(0).int32Data[0];
+        int index=getBubbleRobIndexFromHandle(handle);
+        if (index!=-1)
+        {
+            allBubbleRobs[index].backMovementDuration=0.0f;
+            allBubbleRobs[index].run=true;
+            success=true;
+        }
+        else
+            simSetLastError(LUA_START_COMMAND,"Invalid BubbleRob handle.");
+    }
+    D.pushOutData(CScriptFunctionDataItem(success));
+    D.writeDataToStack(cb->stackID);
+}
+// --------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------
+// simExtBubble_stop
+// --------------------------------------------------------------------------------------
+#define LUA_STOP_COMMAND "simBubble.stop"
+
+const int inArgs_STOP[]={
+    1,
+    sim_script_arg_int32,0,
+};
+
+void LUA_STOP_CALLBACK(SScriptCallBack* cb)
+{
+    CScriptFunctionData D;
+    bool success=false;
+    if (D.readDataFromStack(cb->stackID,inArgs_STOP,inArgs_STOP[0],LUA_STOP_COMMAND))
+    {
+        std::vector<CScriptFunctionDataItem>* inData=D.getInDataPtr();
+        int handle=inData->at(0).int32Data[0];
+        int index=getBubbleRobIndexFromHandle(handle);
+        if (index!=-1)
+        {
+            allBubbleRobs[index].run=false;
+            simSetJointTargetVelocity(allBubbleRobs[index].motorHandles[0],0.0f);
+            simSetJointTargetVelocity(allBubbleRobs[index].motorHandles[1],0.0f);
+            success=true;
+        }
+        else
+            simSetLastError(LUA_STOP_COMMAND,"Invalid BubbleRob handle.");
+    }
+    D.pushOutData(CScriptFunctionDataItem(success));
+    D.writeDataToStack(cb->stackID);
+}
+// --------------------------------------------------------------------------------------
+
+
+SIM_DLLEXPORT unsigned char simStart(void* reservedPointer,int reservedInt)
+{ // This is called just once, at the start of CoppeliaSim.
+    // Dynamically load and bind CoppeliaSim functions:
+    char curDirAndFile[1024];
+#ifdef _WIN32
+    #ifdef QT_COMPIL
+        _getcwd(curDirAndFile, sizeof(curDirAndFile));
+    #else
+        GetModuleFileName(NULL,curDirAndFile,1023);
+        PathRemoveFileSpec(curDirAndFile);
+    #endif
+#elif defined (__linux) || defined (__APPLE__)
+    getcwd(curDirAndFile, sizeof(curDirAndFile));
+#endif
+
+    std::string currentDirAndPath(curDirAndFile);
+    std::string temp(currentDirAndPath);
+
+#ifdef _WIN32
+    temp+="\\coppeliaSim.dll";
+#elif defined (__linux)
+    temp+="/libcoppeliaSim.so";
+#elif defined (__APPLE__)
+    temp+="/libcoppeliaSim.dylib";
+#endif /* __linux || __APPLE__ */
+
+    simLib=loadSimLibrary(temp.c_str());
+    if (simLib==NULL)
+    {
+        printf("simExtBubbleRob: error: could not find or correctly load coppeliaSim.dll. Cannot start the plugin.\n"); // cannot use simAddLog here.
+        return(0); // Means error, CoppeliaSim will unload this plugin
+    }
+    if (getSimProcAddresses(simLib)==0)
+    {
+        printf("simExtBubbleRob: error: could not find all required functions in coppeliaSim.dll. Cannot start the plugin.\n"); // cannot use simAddLog here.
+        unloadSimLibrary(simLib);
+        return(0); // Means error, CoppeliaSim will unload this plugin
+    }
+
+    simRegisterScriptVariable("simBubble","require('simExtBubbleRob')",0);
+
+    // Register the new functions:
+    simRegisterScriptCallbackFunction(strConCat(LUA_CREATE_COMMAND,"@",PLUGIN_NAME),strConCat("number bubbleRobHandle=",LUA_CREATE_COMMAND,"(table_2 motorJointHandles,number sensorHandle,table_2 backRelativeVelocities)"),LUA_CREATE_CALLBACK);
+    simRegisterScriptCallbackFunction(strConCat(LUA_DESTROY_COMMAND,"@",PLUGIN_NAME),strConCat("boolean result=",LUA_DESTROY_COMMAND,"(number bubbleRobHandle)"),LUA_DESTROY_CALLBACK);
+    simRegisterScriptCallbackFunction(strConCat(LUA_START_COMMAND,"@",PLUGIN_NAME),strConCat("boolean result=",LUA_START_COMMAND,"(number bubbleRobHandle)"),LUA_START_CALLBACK);
+    simRegisterScriptCallbackFunction(strConCat(LUA_STOP_COMMAND,"@",PLUGIN_NAME),strConCat("boolean result=",LUA_STOP_COMMAND,"(number bubbleRobHandle)"),LUA_STOP_CALLBACK);
+
+    return(10); // initialization went fine, we return the version number of this plugin (can be queried with simGetModuleName)
+    // version 1 was for CoppeliaSim versions before CoppeliaSim 2.5.12
+    // version 2 was for CoppeliaSim versions before CoppeliaSim 2.6.0
+    // version 5 was for CoppeliaSim versions before CoppeliaSim 3.1.0
+    // version 6 is for CoppeliaSim versions after CoppeliaSim 3.1.3
+    // version 7 is for CoppeliaSim versions after CoppeliaSim 3.2.0 (completely rewritten)
+    // version 8 is for CoppeliaSim versions after CoppeliaSim 3.3.0 (using stacks for data exchange with scripts)
+    // version 9 is for CoppeliaSim versions after CoppeliaSim 3.4.0 (new API notation)
+    // version 10 is for CoppeliaSim versions after CoppeliaSim 4.1.0 (threads via coroutines)
+}
+
+SIM_DLLEXPORT void simEnd()
+{ // This is called just once, at the end of CoppeliaSim
+    unloadSimLibrary(simLib); // release the library
+}
+
+SIM_DLLEXPORT void* simMessage(int message,int* auxiliaryData,void* customData,int* replyData)
+{ // This is called quite often. Just watch out for messages/events you want to handle
+    // This function should not generate any error messages:
+    int errorModeSaved;
+    simGetIntegerParameter(sim_intparam_error_report_mode,&errorModeSaved);
+    simSetIntegerParameter(sim_intparam_error_report_mode,sim_api_errormessage_ignore);
+
+    void* retVal=NULL;
+
+    if (message==sim_message_eventcallback_modulehandle)
+    {
+        if ( (customData==NULL)||(std::string("BubbleRob").compare((char*)customData)==0) ) // is the command also meant for BubbleRob?
+        {
+            float dt=simGetSimulationTimeStep();
+            for (unsigned int i=0;i<allBubbleRobs.size();i++)
+            {
+                if (allBubbleRobs[i].run)
+                { // movement mode
+                    if (simReadProximitySensor(allBubbleRobs[i].sensorHandle,NULL,NULL,NULL)>0)
+                        allBubbleRobs[i].backMovementDuration=3.0f; // we detected an obstacle, we move backward for 3 seconds
+                    if (allBubbleRobs[i].backMovementDuration>0.0f)
+                    { // We move backward
+                        simSetJointTargetVelocity(allBubbleRobs[i].motorHandles[0],-7.0f*allBubbleRobs[i].backRelativeVelocities[0]);
+                        simSetJointTargetVelocity(allBubbleRobs[i].motorHandles[1],-7.0f*allBubbleRobs[i].backRelativeVelocities[1]);
+                        allBubbleRobs[i].backMovementDuration-=dt;
+                    }
+                    else
+                    { // We move forward
+                        simSetJointTargetVelocity(allBubbleRobs[i].motorHandles[0],7.0f);
+                        simSetJointTargetVelocity(allBubbleRobs[i].motorHandles[1],7.0f);
+                    }
+                }
+            }
+        }
+    }
+
+    if (message==sim_message_eventcallback_simulationended)
+    { // simulation ended. Destroy all BubbleRob instances:
+        allBubbleRobs.clear();
+    }
+
+    simSetIntegerParameter(sim_intparam_error_report_mode,errorModeSaved); // restore previous settings
+    return(retVal);
+}
